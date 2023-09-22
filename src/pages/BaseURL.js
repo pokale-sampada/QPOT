8ef767f179f515880c3cd19bbe33a5e9fbc3b4a9
pokale-sampada <142505641+pokale-sampada@users.noreply.github.com>
2023-09-22 12:37:39 +0530

    Delete src directory

--- src/pages/BaseURL.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-
-export default axios.create({
-    baseURL:"https://demo.omfysgroup.com/qpotapp/",
-    PMbaseURL:"https://demo.omfysgroup.com/project-managementQpotAPi/qpot/"
-});
